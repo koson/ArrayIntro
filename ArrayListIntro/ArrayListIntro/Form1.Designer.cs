cbe416c94cddbfa7b5a4bfe7827b7e31c213f503
koson <koson.trachu@gmail.com>
2017-11-22 01:01:30 +0700

    UI and Code added

--- ArrayListIntro/ArrayListIntro/Form1.Designer.cs
+++ ArrayListIntro/ArrayListIntro/Form1.Designer.cs
@@ -28,12 +28,58 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.propertyGrid1 = new System.Windows.Forms.PropertyGrid();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(21, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(139, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Add some number";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(182, 14);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(157, 333);
+            this.textBox1.TabIndex = 1;
+            // 
+            // propertyGrid1
+            // 
+            this.propertyGrid1.LineColor = System.Drawing.SystemColors.ControlDark;
+            this.propertyGrid1.Location = new System.Drawing.Point(361, 14);
+            this.propertyGrid1.Name = "propertyGrid1";
+            this.propertyGrid1.Size = new System.Drawing.Size(285, 333);
+            this.propertyGrid1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(658, 401);
+            this.Controls.Add(this.propertyGrid1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.PropertyGrid propertyGrid1;
     }
 }
 
--- ArrayListIntro/ArrayListIntro/Form1.cs
+++ ArrayListIntro/ArrayListIntro/Form1.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Collections;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
@@ -12,9 +12,23 @@ namespace ArrayListIntro
 {
     public partial class Form1 : Form
     {
+        ArrayList al; // เนื่องจากต้องการสร้าง al ให้เป็นตัวแปรที่เข้าถึงได้จากทุก method ในระดับ class  
         public Form1()
         {
             InitializeComponent();
+            al = new ArrayList(); // ArrayList บรรจุสมาชิกที่เป็น ??
+            propertyGrid1.SelectedObject = al;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            al.Add(new Random().NextDouble());
+            propertyGrid1.Refresh();
+            textBox1.Clear();
+            foreach (var item in al)
+            {
+                textBox1.AppendText( string.Format("{0:N3}", item)  + Environment.NewLine);
+            }
         }
     }
 }
